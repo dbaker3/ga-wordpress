a1a8a8147c865e6e151bc9a752d0da3de85bf327
David <dbaker3@gmail.com>
2016-03-24 18:08:57 +0000

    Changed to analytics.js style tracking

--- ga.php
+++ ga.php
@@ -8,47 +8,38 @@ Author: Jack Weinbender
 Author URI: https://github.com/jackweinbender
 */
 
+/*
+	Modified to use analytics.js - David Baker 2016
+*/
+
 function g_analytics() {
 	if(!is_user_logged_in()){
-		$account_num = 'UA-4045275-3';
+		$tracking_id = '';	   	/***** Make sure to enter your Tracking ID! *****/
 		?>
-			<script type="text/javascript">
-				var _gaq = _gaq || [];
-				_gaq.push(['_setAccount', '<?php echo $account_num ?>']);
-				_gaq.push(['_trackPageview']);
-			</script>
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			
+			ga('create', '<?php echo $tracking_id ?>', 'auto');
+			ga('send', 'pageview');
+		</script>
 		<?php } 
 }
 add_action( 'wp_head', 'g_analytics' );
 
 function g_track_downloads(){
 	if(!is_user_logged_in()){ ?>
-		<script type="text/javascript">
+		<script>
 			/** Tracks clicked links with ga-download class **/
-				jQuery('.ga-download').mouseup(function(){
-					var category = 'Documents';
-					var action = 'Downloads';
-					var label = jQuery( this ).attr('title');
-					_gaq.push(['_trackEvent', category , action, label]);
-				});
+			jQuery('.ga-download').mouseup(function(){
+				var category = 'Documents';
+				var action = 'Downloads';
+				var label = jQuery( this ).attr('title');
+				ga('send', 'event', category , action, label);
+			});
 		</script>
 	<?php }
 }
-
-add_action( 'wp_head', 'g_track_downloads' );
-
-
-function ga_add_ga_js() {?>
-<script type="text/javascript">
-	(function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-</script><?php
-	}
-
-add_action('admin_footer', 'ga_add_ga_js');
-add_action('wp_footer', 'ga_add_ga_js');
-
-?>
+add_action( 'wp_footer', 'g_track_downloads' );
